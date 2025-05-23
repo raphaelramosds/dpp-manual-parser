03e78cb056f7e2465e88f622e9fee94aa3d79787
Raphael Ramos <raphael.opensource@gmail.com>
2025-05-23 18:11:41 -0300

    fix

--- index.php
+++ index.php
@@ -12,54 +12,55 @@ include_once 'env.php';
 
 // ---------- INPUTS
 
-$report = 'Manual_NPR';
-$excel = './assets/excel/006_CNPJ_AAAAMMDDHHMMSS_VER.xls';
-$pdf = './assets/pdf/NPP.pdf';
+$report = 'rap';
+// $excel = './assets/excel/006_CNPJ_AAAAMMDDHHMMSS_VER.xls';
+// $pdf = './assets/pdf/NPP.pdf';
 
 // ---------- PDF to TXT conversion
 
-$pdfh = new PdfToTxt($pdf, TXT_OUTPUT_DIR, $report);
-$pdfh->setReloadPdf(false);
-$pdfh->convert();
+// $pdfh = new PdfToTxt($pdf, TXT_OUTPUT_DIR, $report);
+// $pdfh->setReloadPdf(false);
+// $pdfh->convert();
 
 // ---------- GET FIELDS AND SECTIONS FROM XLSX
 
-$spreadsheet_fields = (new SpreadsheetContext($excel))->getFields();
+// $spreadsheet_fields = (new SpreadsheetContext($excel))->getFields();
 
 // ---------- GET FIELDS FROM TXT
 
 $textContext = new TxtContext(TXT_OUTPUT_DIR . "/$report.txt"); 
-$textContext->setSplitter(new FieldFormatSplitter());
+$textContext->setSplitter(new TitleSplitter());
 $txtFields = $textContext->parse();
+echo var_dump($txtFields);
 
 // ---------- TXT and XLSX mapping
 $mapping = [];
-foreach ($spreadsheet_fields as $key => $fields) {
-    array_walk($fields, function ($field) use (&$mapping, $txtFields) {
-        if (!array_key_exists($field, $txtFields)) {
-            echo "Spreadsheet field $field does not match any fields on TXT" . PHP_EOL;
-            $mapping[$field] = [
-                'field' => $field,
-                'type' => 'UNKNOWN',
-                'length' => 'UNKNOWN',
-                'required' => 'UNKNOWN',
-                'filling_rule' => 'UNKNOWN'
-            ];
-        } else {
-            $mapping[$field] = $txtFields[$field];
-        }
-    });
-}
+// foreach ($spreadsheet_fields as $key => $fields) {
+//     array_walk($fields, function ($field) use (&$mapping, $txtFields) {
+//         if (!array_key_exists($field, $txtFields)) {
+//             echo "Spreadsheet field $field does not match any fields on TXT" . PHP_EOL;
+//             $mapping[$field] = [
+//                 'field' => $field,
+//                 'type' => 'UNKNOWN',
+//                 'length' => 'UNKNOWN',
+//                 'required' => 'UNKNOWN',
+//                 'filling_rule' => 'UNKNOWN'
+//             ];
+//         } else {
+//             $mapping[$field] = $txtFields[$field];
+//         }
+//     });
+// }
 
-foreach ($mapping as $section => $fields) 
-{
-    array_walk($fields, function ($field) use (&$mapping, $section, $fields) {
-        $mapping[$section]['required'] = $fields['required'];
-        $mapping[$section]['type'] = strtoupper($fields['type']);
-        $mapping[$section]['length'] = $fields['length'];
-        $mapping[$section]['filling_rule'] = $fields['filling_rule'];
-    });
-}
+// foreach ($mapping as $section => $fields) 
+// {
+//     array_walk($fields, function ($field) use (&$mapping, $section, $fields) {
+//         $mapping[$section]['required'] = $fields['required'];
+//         $mapping[$section]['type'] = strtoupper($fields['type']);
+//         $mapping[$section]['length'] = $fields['length'];
+//         $mapping[$section]['filling_rule'] = $fields['filling_rule'];
+//     });
+// }
 
 // ---------- CREATE TXT WITH FIELDS AND THEIR RULES
 
@@ -86,14 +87,11 @@ fwrite($output, PHP_EOL);
 
 foreach ($mapping as $field => $rules)
 {
-    // $content = <<<EOF
-    // FIELD "$field" IS {$rules['type']}({$rules['length']}) {$rules['required']};
-
-    // EOF;
     $content = <<<EOF
-    FIELD "$field" IS {$rules['type']} {$rules['required']};
+    FIELD "$field" IS {$rules['type']}({$rules['length']}) {$rules['required']};
 
     EOF;
+
     fwrite($output, $content);
 }
 
